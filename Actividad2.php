e7cdb58ce4c90f04c440426def9407bb83aeba41
emilianole6312 <emilianole0312@comunidad.unam.mx>
2020-05-14 13:03:32 -0500

    Feat: Añadi el archivo Actividad2.php

---
+++ Actividad2.php
@@ -0,0 +1,8 @@
+<?php
+  echo "Lopez Esquivel Emiliano <br> ";
+  echo "Las dos cosas que mas me han gustado del curso son: <br>";
+  echo "<ul>";
+    echo "<li>Karel</li>";
+    echo "<li>PHP</li>";
+  echo "</ul>";
+ ?>
